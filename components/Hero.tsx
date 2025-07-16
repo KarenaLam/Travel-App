9e8bb6f04d8fbe91a3061bc564ec1fad2571c731
Karena Lam <lamkare2@msu.edu>
2025-07-15 22:47:32 -0400

    7/15/2025-Finshed Hero component and completed part of Camp component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,8 +62,9 @@ const Hero = () => {
             </div>
         </div>
         <div className='relative flex flex-1 items-start'>
-            <div className='relative z-20 w-[268px] flex-col gap-8 rounded-3xl [background-color:#292C27]
+            <div className='relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl [background-color:#292C27]
             px-7 py-8'>
+
                 <div className='flex flex-col'>
                     <div className='flexBetween'>
                         <p className='regular-16 [color:#A2A2A2]'>Location</p>
@@ -74,11 +75,18 @@ const Hero = () => {
                             height={24}
                         />
                     </div>
-                    <div className='flexBetween'>
-                        <div className='flex flex-col'>
-                            <p className='regular-16 block [color:#A2A2A2]'>Distance</p>
-                            <p className='bold-20 text-white'>178.28 mi</p>
-                        </div>
+                    <p className='bold-20 text-white'>Aguas Calientes</p>
+                </div>
+
+                <div className='flexBetween'>
+                    <div className='flex flex-col'>
+                        <p className='regular-16 block [color:#A2A2A2]'>Distance</p>
+                        <p className='bold-20 text-white'>178.28 mi</p>
+                    </div>
+
+                    <div className='flex flex-col'>
+                        <p className='regular-16 block [color:#A2A2A2]'>Elevation</p>
+                        <p className='bold-20 text-white'>2.040 km</p>
                     </div>
                 </div>
             </div>
